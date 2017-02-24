bac1d9958f777f787065ee74fb9a5abdb922f04e
Andrew <brownmoose3q@gmail.com>
2017-02-24 12:48:11 +0300

    Patch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const ToastAndroid = require('react-native').NativeModules.ToastAndroid;
 const PlayerAndroid = require('react-native').NativeModules.PlayerAndroid;
 const LightSensorAndroid = require('react-native').NativeModules.LightSensorAndroid;
-const P2PAndroid = require('react-native').NativeModules.P2pAndroid;
+const P2pAndroid = require('react-native').NativeModules.P2pAndroid;
 
 export default { ToastAndroid,
                  PlayerAndroid,
